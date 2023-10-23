c10c914001c0c6c144d4e7163b5d223d34c954b5
Nelson Leme de Almeida Junior <sonjunior@live.com>
2023-10-23 17:34:40 +0000

    operador unario de resolucao de escopo. limpeza do codigo.

--- operadorUnarioDeResolucaoDeEscopo.cxx
+++ operadorUnarioDeResolucaoDeEscopo.cxx
@@ -3,7 +3,6 @@ using std::cout;
 using std::endl;
 
 int number = 7;
-int &numberRef = ::number;
 
 int main(){
 
@@ -18,9 +17,5 @@ int main(){
 
     cout << "\n fora do bloco do main" << number << endl;
     cout << "\n fora do bloco do main com ::" << ::number << endl;
-    numberRef = 10;
-    cout << "\n dentro do bloco do main $numberRef: " << ::number << endl;
-    cout << "\n &number" << &number << endl;
-    cout << "\n &numerRef" << &numberRef << endl;
     return 0;
 }
