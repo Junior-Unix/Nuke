c8ada403f641d7c48b73bda2f6a64b2f48b64e3a
Junior-Unix <sonjuniot@live.com>
2023-10-28 23:27:00 -0300

    Erro linker GradeBook.h

--- GradeBook/GradeBook.h
+++ GradeBook/GradeBook.h
@@ -6,23 +6,20 @@ using std::endl;
 using std::string;
 
 class GradeBook{
-
-public:
-
-    GradeBook(string name){
-        setCourseName(name);
-    }
-    void setCourseName(string name){
-        courseName = name;
-    }
-    string getCourseName(){
-        return courseName;
-    }
-    void displayMessage(){
-        cout << "Welcome to the grade book for\n" << getCourseName()
+    public:
+        GradeBook(string name){
+            setCourseName(name);
+        }
+        void setCourseName(string name){
+            courseName = name;
+        }
+        string getCourseName(){
+            return courseName;
+        }
+        void displayMessage(){
+            cout << "Welcome to the grade book for\n" << getCourseName()
             << "!" << endl;
-    }
-
-private:
-    string courseName;
+        }
+    private:
+        string courseName;
 };
