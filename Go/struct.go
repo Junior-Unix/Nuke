485bb7ca236f9e495cd66b95d561d68ddef40472
Nelson Junior <sonjunior@live.com>
2023-11-22 17:16:33 +0000

    composição.

--- Go/struct.go
+++ Go/struct.go
@@ -1,32 +1,89 @@
-package main
 
-import (
-	"fmt"
-	"strings"
-)
 
-type pessoa struct {
-	nome string
-	sobrenome string
-}
 
-func (p pessoa) getNomeCompleto() string {
-	return p.nome + " " + p.sobrenome
-}
 
-func (p *pessoa) setNomeCompleto(nomeCompleto string) {
-	partes := strings.Split(nomeCompleto, " ")
-	p.nome = partes[0]
-	p.sobrenome = partes[1]
-}
+// package main
 
-func main() {
-	p1 := pessoa{"Pedro", "Silva"}
-	fmt.Println(p1.getNomeCompleto())
+// import ( "fmt" )
 
-	p1.setNomeCompleto("Maria Pereira")
-	fmt.Println(p1.getNomeCompleto())
-}
+// type carro struct {
+// 	nome string
+// 	velocidadeAtual int
+// }
+
+// type ferrari struct {
+// 	carro
+// 	turboLigado bool
+// }
+
+
+// func main() {
+// 	f := ferrari{}
+// 	f.nome = "F40"
+// 	f.velocidadeAtual = 0
+// 	f.turboLigado = true
+
+// 	fmt.Printf("Ferrari %s turbo ligado? %v\n", f.nome, f.turboLigado)
+// 	fmt.Println(f.carro)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// type carro struct {
+// 	nome string
+// 	velocidadeAtual int
+// }
+
+// type ferrari struct {
+// 	carro
+// 	turboLigado bool
+// }
+
+// func main() {
+// 	f := ferrari{}
+// 	f.nome = "F40"
+// 	f.velocidadeAtual = 0
+// 	f.turboLigado = true
+
+// 	fmt.Printf("A ferraru %s turbina? %v\n", f.nome, f.turboLigado)
+// 	fmt.Println(f)
+// }
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// type pessoa struct {
+// 	nome string
+// 	sobrenome string
+// }
+
+// func (p pessoa) getNomeCompleto() string {
+// 	return p.nome + " " + p.sobrenome
+// }
+
+// func (p *pessoa) setNomeCompleto(nomeCompleto string) {
+// 	partes := strings.Split(nomeCompleto, " ")
+// 	p.nome = partes[0]
+// 	p.sobrenome = partes[1]
+// }
+
+// func main() {
+// 	p1 := pessoa{"Pedro", "Silva"}
+// 	fmt.Println(p1.getNomeCompleto())
+
+// 	p1.setNomeCompleto("Maria Pereira")
+// 	fmt.Println(p1.getNomeCompleto())
+// }
 
 // package main
 
