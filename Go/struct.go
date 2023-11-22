b2849f20507707b5214873c41a0e81d1c646fbfa
Nelson Junior <sonjunior@live.com>
2023-11-22 22:09:24 +0000

    -

--- Go/struct.go
+++ Go/struct.go
@@ -28,13 +28,21 @@ func (p produto) toString() string {
 	return fmt.Sprintf("%s = R$ %.2f", p.nome, p.preco)
 }
 
-func imprimir( x imprimivel) {
+func imprimir(x imprimivel) {
 	fmt.Println(x.toString())
 }
 
 func main() {
 	var coisa imprimivel = pessoa{"Roberso" , "Silva"}
 	fmt.Println(coisa.toString())
+
+	coisa = produto{"Calça Jeans", 79.90}
+	fmt.Println(coisa.toString())
+	imprimir(coisa)
+
+	p2 := produto{"Calça Jeans", 179.0}
+	imprimir(p2)
+
 }
 
 
