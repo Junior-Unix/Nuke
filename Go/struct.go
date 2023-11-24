ef5d4f542ac7951a6b3521a2b959d434f599ce91
Nelson Junior <sonjunior@live.com>
2023-11-24 20:51:44 +0000

    runtime cpus.

--- Go/struct.go
+++ Go/struct.go
@@ -1,25 +1,38 @@
+//Quantidade de processadores
 package main
 
 import (
-	"math"
+
+	"fmt"
+	"runtime"
 )
 
-//Ponto representa uma coordenada no plano cartesiano.
-type Ponto struct {
-	x float64
-	y float64
+func main() {
+	fmt.Println(runtime.NumCPU())
 }
 
-func catetos(a, b Ponto) (cx, cy float64) {
-	cx = math.Abs(b.x - a.x)
-	cy = math.Abs(b.y - a.y)
-	return
-}
-//Distancia é responsável por calcular a distância linear entre dois pontos
-func Distancia(a, b Ponto) float64 {
-	cx, cy := catetos( a, b)
-	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))	
-}
+// package main
+
+// import (
+// 	"math"
+// )
+
+// //Ponto representa uma coordenada no plano cartesiano.
+// type Ponto struct {
+// 	x float64
+// 	y float64
+// }
+
+// func catetos(a, b Ponto) (cx, cy float64) {
+// 	cx = math.Abs(b.x - a.x)
+// 	cy = math.Abs(b.y - a.y)
+// 	return
+// }
+// //Distancia é responsável por calcular a distância linear entre dois pontos
+// func Distancia(a, b Ponto) float64 {
+// 	cx, cy := catetos( a, b)
+// 	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))	
+// }
 
 
 
