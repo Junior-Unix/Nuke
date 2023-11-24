8623763f6e2f83d4329aca149057bccd4de53698
Nelson Junior <sonjunior@live.com>
2023-11-24 21:26:25 +0000

    chanel.

--- Go/struct.go
+++ Go/struct.go
@@ -1,27 +1,45 @@
+//Chanel1
 package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func fale(pessoa, texto string, qtde int) {
-	for i := 0; i < qtde; i++ {
-		time.Sleep(time.Second)
-		fmt.Printf("%s: %s (iteração %d)\n", pessoa, texto, i+1)
-	}
-}
-
 func main() {
-	// go fale("Maria", "Pq não fala?", 500)
-	// go fale("Joao", "Só depois!", 500)
-	// fmt.Println("Fim!")
+	ch := make(chan int, 1)
 
-		go fale("Maria", "Entendi!!!", 10)
-		fale("João", "Parabéns!", 5)
+	ch <- 1 //enviando dados para o canal( escrita)
+	<-ch 
 
-
+	ch <- 2
+	fmt.Println(<-ch)
 }
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func fale(pessoa, texto string, qtde int) {
+// 	for i := 0; i < qtde; i++ {
+// 		time.Sleep(time.Second)
+// 		fmt.Printf("%s: %s (iteração %d)\n", pessoa, texto, i+1)
+// 	}
+// }
+
+// func main() {
+// 	// go fale("Maria", "Pq não fala?", 500)
+// 	// go fale("Joao", "Só depois!", 500)
+// 	// fmt.Println("Fim!")
+
+// 		go fale("Maria", "Entendi!!!", 10)
+// 		fale("João", "Parabéns!", 5)
+
+
+// }
 
 
 // //Quantidade de processadores
