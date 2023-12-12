14f0c9c77db25985db4cc49dbd0b261cb2990192
Nelson Junior <sonjunior@live.com>
2023-12-12 00:46:37 +0000

    out.gif

--- Go/estrutura.go
+++ Go/estrutura.go
@@ -1,78 +1,126 @@
-// Server DB. Rest metodo GET.
-package main
-
-import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
-	"strings"
-
-	_ "github.com/go-sql-driver/mysql"
+//gopl.io/ch1/lissajous // Lissajous gera animações GIF de figuras de Lissajous aleatórias 
+package main 
+import ( 
+	"image"
+	"image/color" 
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+	)
+var palette = []color.Color{color.White, color.Black} 
+const ( 
+	whiteIndex = 0   // primeira cor da paleta 
+	blackIndex = 1   // próxima cor da paleta 
 )
 
-// Usuario :)
-type Usuario struct {
-	ID   int    `json:"id"`
-	Nome string `json:"nome"`
+func main() { 
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+} 
+func lissajous(out io.Writer) {
+	const ( cycles  = 5     // número de revoluções completas do oscilador x 
+		res     = 0.001 // resolução angular 
+		size    = 100   // canvas da imagem cobre de [-size..+size] 
+		nframes = 64    // número de quadros da animação 
+		delay   = 8     // tempo entre quadros em unidades de 10ms 
+	) 
+	freq := rand.Float64() * 3.0   // frequência relativa do oscilador y 
+	anim := gif.GIF{LoopCount: nframes} 
+	phase := 0.0   // diferença de fase 
+	for i := 0; i < nframes; i++ { 
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1) 
+		img := image.NewPaletted(rect, palette) 
+		for t := 0.0; t < cycles*2*math.Pi; t += res { 
+			x := math.Sin(t) 
+			y := math.Sin(t*freq + phase) 
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex) 
+		} 
+		phase += 0.1 
+		anim.Delay = append(anim.Delay, delay) 
+		anim.Image = append(anim.Image, img) 
+		} 
+		gif.EncodeAll(out, &anim) // NOTA: ignorando erros de codificação 
 }
 
-// UsuarioHandler analisa o request e delega para função adequada
-func UsuarioHandler(w http.ResponseWriter, r *http.Request) {
-	sid := strings.TrimPrefix(r.URL.Path, "/usuarios/")
-	id, _ := strconv.Atoi(sid)
 
-	switch {
-	case r.Method == "GET" && id > 0:
-		usuarioPorID(w, r, id)
-	case r.Method == "GET":
-		usuarioTodos(w, r)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "Desculpa... :(")
-	}
-}
+// // Server DB. Rest metodo GET.
+// package main
 
-func usuarioPorID(w http.ResponseWriter, r *http.Request, id int) {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1)/go")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+// import (
+// 	"database/sql"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"strconv"
+// 	"strings"
 
-	var u Usuario
-	db.QueryRow("select id, nome from usuarios where id = ?", id).Scan(&u.ID, &u.Nome)
+// 	_ "github.com/go-sql-driver/mysql"
+// )
 
-	json, _ := json.Marshal(u)
+// // Usuario :)
+// type Usuario struct {
+// 	ID   int    `json:"id"`
+// 	Nome string `json:"nome"`
+// }
 
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(json))
-}
+// // UsuarioHandler analisa o request e delega para função adequada
+// func UsuarioHandler(w http.ResponseWriter, r *http.Request) {
+// 	sid := strings.TrimPrefix(r.URL.Path, "/usuarios/")
+// 	id, _ := strconv.Atoi(sid)
 
-func usuarioTodos(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1)/go")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+// 	switch {
+// 	case r.Method == "GET" && id > 0:
+// 		usuarioPorID(w, r, id)
+// 	case r.Method == "GET":
+// 		usuarioTodos(w, r)
+// 	default:
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprintf(w, "Desculpa... :(")
+// 	}
+// }
 
-	rows, _ := db.Query("select id, nome from usuarios")
-	defer rows.Close()
+// func usuarioPorID(w http.ResponseWriter, r *http.Request, id int) {
+// 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1)/go")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
 
-	var usuarios []Usuario
-	for rows.Next() {
-		var usuario Usuario
-		rows.Scan(&usuario.ID, &usuario.Nome)
-		usuarios = append(usuarios, usuario)
-	}
+// 	var u Usuario
+// 	db.QueryRow("select id, nome from usuarios where id = ?", id).Scan(&u.ID, &u.Nome)
 
-	json, _ := json.Marshal(usuarios)
+// 	json, _ := json.Marshal(u)
 
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, string(json))
-}
+// 	w.Header().Set("Content-Type", "application/json")
+// 	fmt.Fprint(w, string(json))
+// }
+
+// func usuarioTodos(w http.ResponseWriter, r *http.Request) {
+// 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1)/go")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+
+// 	rows, _ := db.Query("select id, nome from usuarios")
+// 	defer rows.Close()
+
+// 	var usuarios []Usuario
+// 	for rows.Next() {
+// 		var usuario Usuario
+// 		rows.Scan(&usuario.ID, &usuario.Nome)
+// 		usuarios = append(usuarios, usuario)
+// 	}
+
+// 	json, _ := json.Marshal(usuarios)
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	fmt.Fprint(w, string(json))
+// }
 
 
 // //Dinâmico
