a225c0d27e93b46cf3a2660e884818e60f1b85b3
Nelson Junior <sonjunior@live.com>
2024-02-18 18:16:05 +0000

    tem que dar push no repositorio pra atualizar.

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,13 @@
-var numFlavors = ["vanilla", "butterschot"];
-console.log(numFlavors);
-console.log(numFlavors.length);
-console.log(numFlavors[1]);
+var scores = [60, 50, 60, 58, 54, 54,
+              58, 50, 52, 54, 48, 69,
+              34, 55, 51, 52, 44, 51,
+              69, 64, 66, 55, 52, 61,];
+
+var output;
+var i = 0;
+
+while( i > scores.length){
+    output = "Bobble solution #" + i + " score: " + scores[i];
+    console.log(output);
+    i = i + 1;
+}
