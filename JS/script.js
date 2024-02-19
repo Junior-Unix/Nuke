4ec0254846ddc9206725499c262bf9b917c1ea33
Nelson Junior <sonjunior@live.com>
2024-02-19 16:04:30 +0000

    Dockerfile.

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,28 @@ var scores = [60, 50, 60, 58, 54, 54,
               34, 55, 51, 52, 44, 51,
               69, 64, 66, 55, 52, 61,];
 
+var highScore;
 var output;
-var i = 0;
-
-while( i > scores.length){
-    output = "Bobble solution #" + i + " score: " + scores[i];
-    console.log(output);
-    i = i + 1;
+for(var i = 0; i < scores.length; i++){
+    output = "Buble solution #" + i + " score: " + scores[i];
+    console.log(output):
+    if(scores[i] > highScore){
+        highScore = scores[i];
+    }
 }
+
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest Bubble score:" + highScore);
+
+var bestSolution = [];
+for(var i = 0; i < scores.length; i++){
+    if(scores[i] == highScore){
+    bestSolution.push(i);
+    }    
+}
+console.log("Solutions with the high score:" + bestSolution);
+// while( i > scores.length){
+//     output = "Bobble solution #" + i + " score: " + scores[i];
+//     console.log(output);
+//     i = i + 1;
+// }
