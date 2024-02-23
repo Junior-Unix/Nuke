39576890de137f30717abe279a528db7561dfaeb
Junior-Unix <sonjunior@live.com>
2024-02-23 18:04:03 -0300

    css

--- JS/getElement.js
+++ JS/getElement.js
@@ -1,4 +1,10 @@
-var article = document.getElementById('article');
-console.log(article);
+// var article = document.getElementsByTagName('p');
+// console.log(article);
 
-article.style.backgroundColor = 'red'
+// article.style.backgroundColor = 'red'
+var image = document.getElementById('img');
+console.log(image);
+
+image.onclick = function(){
+    var elemento = document.getElementById()
+}
