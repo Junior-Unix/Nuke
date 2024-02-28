45270ea9094f982f558940fa33703f3354f454ca
Junior-Unix <sonjunior@live.com>
2024-02-28 17:58:51 -0300

    objects

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,44 @@
-var fiat={
-    make: "Fiat",
-    model: "500",
-    starded: false,
-    fuel: 0,
+var access = document.getElementById("code9");
+var code = access.innerHTML;
+code = code + " midnight";
+alert(code);
 
-    start: function(){
-        this.starded=true;
-    },
-    stop: function(){
-        this.started=false;
-    },
-    drive: function(){
-        if(this.started){
-            if(this.fuel > 0){
-                alert(this.make.make + " " + this.model + " goes zoom zoom!");
-            this.fuel = this.fuel - 1;    
-            }else{
-                alert("Uh oh, out of fuel.");
-                this.stop();
-            }
-        }else{
-            alert("You need to start the engine first.");
-        }
-    },
-    addFuel: function(amount){
-        this.fuel=this.fuel + amount;
-    }
-};
 
-fiat.start();
-fiat.drive();
-fiat.addFuel(5);
-fiat.start();
-fiat.drive();
+// var fiat={
+//     make: "Fiat",
+//     model: "500",
+//     starded: false,
+//     fuel: 0,
+
+//     start: function(){
+//         this.starded=true;
+//     },
+//     stop: function(){
+//         this.started=false;
+//     },
+//     drive: function(){
+//         if(this.started){
+//             if(this.fuel > 0){
+//                 alert(this.make + " " + this.model + " goes zoom zoom!");
+//             this.fuel = this.fuel - 1;    
+//             }else{
+//                 alert("Uh oh, out of fuel.");
+//                 this.stop();
+//             }
+//         }else{
+//             alert("You need to start the engine first.");
+//         }
+//     },
+//     addFuel: function(amount){
+//         this.fuel=this.fuel + amount;
+//     }
+// };
+
+// fiat.start();
+// fiat.drive();
+// fiat.addFuel(5);
+// fiat.start();
+// fiat.drive();
 
 
 // var fiat={
