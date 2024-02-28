09791b2b5f545913a2d450ae2bb7c5bddddab84d
Junior-Unix <sonjunior@live.com>
2024-02-28 16:12:30 -0300

    addFuel.

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,68 @@
 var fiat={
     make: "Fiat",
-    started: false,
+    model: "500",
+    starded: false,
+    fuel: 0,
+
     start: function(){
-        this.started=true;
+        this.starded=true;
     },
     stop: function(){
         this.started=false;
     },
     drive: function(){
         if(this.started){
-            alert("Zoom zoom!");
+            if(this.fuel > 0){
+                alert(this.make.make + " " + this.model + " goes zoom zoom!");
+            this.fuel = this.fuel - 1;    
+            }else{
+                alert("Uh oh, out of fuel.");
+                this.stop();
+            }
         }else{
-            alert("You need to start the engine first,");
+            alert("You need to start the engine first.");
         }
+    },
+    addFuel: function(amount){
+        this.fuel=this.fuel + amount;
     }
 };
-fiat.drive();
+
 fiat.start();
 fiat.drive();
-fiat.stop();
+fiat.addFuel(5);
+fiat.start();
 fiat.drive();
 
 
+// var fiat={
+//     make: "Fiat",
+//     started: false,
+    
+//     start: function(){
+//         this.started=true;
+//     },
+    
+//     stop: function(){
+//         this.started=false;
+//     },
+    
+//     drive: function(){
+//         if(this.started){
+//             alert("Zoom zoom!");
+//         }else{
+//             alert("You need to start the engine first!");
+//         }
+//     }
+    
+// };
+// fiat.drive();
+// fiat.start();
+// fiat.drive();
+// fiat.stop();
+// fiat.drive();
 
+
+
+// const a = "💀"
+// console.log(a);
