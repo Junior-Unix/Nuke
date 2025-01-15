07ce68758288cd65165329417e05275f96429bc5
Junior-Unix <sonjunior@live.com>
2025-01-15 15:37:09 +0000

    Caracteres simples.

---
+++ Regex/Caracteres/cuidados.js
@@ -0,0 +1,9 @@
+const textoUmaLinha = "..."; // Aspas simples ou duplas
+
+const textoMultiLinha = `
+    linha 1
+    linha 2
+`; // Template String
+
+// Cuidado com o tab!
+console.log('	'.match(/\s/g)); // Saída: [ '	' ]
