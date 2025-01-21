b4c73c5becf4636a9a14d06bcb5d3961ba4e53c3
Junior-Unix <sonjunior@live.com>
2025-01-21 02:35:37 +0000

    linha 41.

--- C/contaArgc.c
+++ C/contaArgc.c
@@ -38,7 +38,7 @@ main( int argc, char *argv[]){
 	while ( fgets( s, MAX_LIN+1, fp) != NULL){
 		s[strlen(s)-1] = '\0';
 			if( mostra_linhas)
-				fprintf( stdout, "%d: %d\n", ++nlinha, strlen(s));
+				fprintf( stdout, "%d: %d\n", ++n_linha, strlen(s));
 
 	}
 	fclose(fp);
